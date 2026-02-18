d554eaa51ec132f393acd22d5c8b52b576acb018
JhonCooktoday <jhonramos3456@gmail.com>
2026-02-17 19:02:33 -0500

    agregando data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO producto(nombre, precio) VALUES ('Laptop', 3500);
+INSERT INTO producto(nombre, precio) VALUES ('Teclado', 120);
+INSERT INTO producto(nombre, precio) VALUES ('Mouse', 80);
